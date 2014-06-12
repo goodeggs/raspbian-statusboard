df9440ec3b2fa4a069380f26692753e6a4557477
Bob Zoller <bob@zoller.us>
2014-06-12 02:10:34 -0700

    better shift_state and raspbian-statusboard

--- files/shift_state.c
+++ files/shift_state.c
@@ -1,27 +1,17 @@
-#include <sys/ioctl.h> 
-#include <asm/ioctls.h> 
-#include <stdio.h> 
-#include <fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) { 
-    int fd; 
-    char shift_state; 
+#include <sys/ioctl.h>
 
-    fd = open("/dev/console", O_RDWR); 
+int main()
+{
+  char shift_state;
 
-    /* 
-     * From console_ioctl: 
-     *        TIOCLINUX, subcode=6 
-     *               argp  points  to  a char which is set to the value of the kernel 
-     *               variable shift_state.  (Since 1.1.32.) 
-     */ 
-
-    shift_state = 6; 
-    ioctl(fd, TIOCLINUX, &shift_state); 
-
-    printf("shift_state = %i\n", (int)shift_state); 
-
-    close(fd); 
-
-    return shift_state; 
+  shift_state = 6;
+  if (ioctl(0, TIOCLINUX, &shift_state) < 0) {
+    perror("ioctl TIOCLINUX 6 (get shift state)");
+    exit(1);
+  }
+  printf("%x\n", shift_state);
+  return 0;
 }
